38b3d43cf452af9bc98e3f92785e35cb89edea21
Narine686 <160075788+Narine686@users.noreply.github.com>
2025-04-11 10:36:43 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -16,11 +16,22 @@ setup(
 
 if __name__ is not None:
     print(r"""
- | |        | | ( )     |  __ \            | (_)    | |
- | |     ___| |_|/ ___  | |__) | __ ___  __| |_  ___| |_
- | |    / _ \ __| / __| |  ___/ '__/ _ \/ _` | |/ __| __|
- | |___|  __/ |_  \__ \ | |   | | |  __/ (_| | | (__| |_
- |______\___|\__| |___/ |_|   |_|  \___|\__,_|_|\___|\__|
+PPPPPPPPPPPPPPPPP                                       kkkkkkkk                                                                      
+P::::::::::::::::P                                      k::::::k                                                                      
+P::::::PPPPPP:::::P                                     k::::::k                                                                      
+PP:::::P     P:::::P                                    k::::::k                                                                      
+  P::::P     P:::::P  ooooooooooo       cccccccccccccccc k:::::k    kkkkkkk   mmmmmmm    mmmmmmm     aaaaaaaaaaaaa  nnnn  nnnnnnnn    
+  P::::P     P:::::Poo:::::::::::oo   cc:::::::::::::::c k:::::k   k:::::k  mm:::::::m  m:::::::mm   a::::::::::::a n:::nn::::::::nn  
+  P::::PPPPPP:::::Po:::::::::::::::o c:::::::::::::::::c k:::::k  k:::::k  m::::::::::mm::::::::::m  aaaaaaaaa:::::an::::::::::::::nn 
+  P:::::::::::::PP o:::::ooooo:::::oc:::::::cccccc:::::c k:::::k k:::::k   m::::::::::::::::::::::m           a::::ann:::::::::::::::n
+  P::::PPPPPPPPP   o::::o     o::::oc::::::c     ccccccc k::::::k:::::k    m:::::mmm::::::mmm:::::m    aaaaaaa:::::a  n:::::nnnn:::::n
+  P::::P           o::::o     o::::oc:::::c              k:::::::::::k     m::::m   m::::m   m::::m  aa::::::::::::a  n::::n    n::::n
+  P::::P           o::::o     o::::oc:::::c              k:::::::::::k     m::::m   m::::m   m::::m a::::aaaa::::::a  n::::n    n::::n
+  P::::P           o::::o     o::::oc::::::c     ccccccc k::::::k:::::k    m::::m   m::::m   m::::ma::::a    a:::::a  n::::n    n::::n
+PP::::::PP         o:::::ooooo:::::oc:::::::cccccc:::::ck::::::k k:::::k   m::::m   m::::m   m::::ma::::a    a:::::a  n::::n    n::::n
+P::::::::P         o:::::::::::::::o c:::::::::::::::::ck::::::k  k:::::k  m::::m   m::::m   m::::ma:::::aaaa::::::a  n::::n    n::::n
+P::::::::P          oo:::::::::::oo   cc:::::::::::::::ck::::::k   k:::::k m::::m   m::::m   m::::m a::::::::::aa:::a n::::n    n::::n
+PPPPPPPPPP            ooooooooooo       cccccccccccccccckkkkkkkk    kkkkkkkmmmmmm   mmmmmm   mmmmmm  aaaaaaaaaa  aaaa nnnnnn    nnnnnn
 
 PockMan Installed!
 Ready to predict binding sites!
